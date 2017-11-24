99999b8a081897c18203c062cb6ad91a3a883aaf
Lillios <33210452+Lillios@users.noreply.github.com>
2017-11-24 11:37:40 +0900

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Hello{
+	public static void main(String[] args){
+		JFrame j = new JFrame("Hello");
+		j.setBounds(500,500,500,500);
+		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+
+		JButton jb = new JButton("CLECK ME");
+
+		JPanel jp = new JPanel();
+		jp.add(jb);
+
+		Container cp = j.getContentPane();
+		cp.add(jp,BorderLayout.CENTER);
+
+		j.setVisible(true);
+
+
+	}
+}
