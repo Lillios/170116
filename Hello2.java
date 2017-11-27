a19cd9c8d7cb400b12a1ac82269fa7eb6646ce78
Lillios <33210452+Lillios@users.noreply.github.com>
2017-11-27 10:18:43 +0900

    Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Hello2{
+	 public static void main(String[] args) {
+		JFrame j = new JFrame("Hello2");
+		j.setBounds(500,500,500,500);
+		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
+		j.setVisible(true);
+
+	}
+}
