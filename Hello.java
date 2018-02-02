a57cd552fc7eaf021af7e778eb3e42aa49cada3a
Lillios <33210452+Lillios@users.noreply.github.com>
2018-02-02 09:27:48 +0900

    test commit

--- Hello.java
+++ Hello.java
@@ -2,20 +2,9 @@ import java.awt.*;
 import javax.swing.*;
 public class Hello{
 	public static void main(String[] args){
-		JFrame j = new JFrame("Hello");
+		JFrame j = new JFrame();
 		j.setBounds(500,500,500,500);
-		j.setDefaultCloseOperation(j.EXIT_ON_CLOSE);
-
-		JButton jb = new JButton("CLECK ME");
-
-		JPanel jp = new JPanel();
-		jp.add(jb);
-
-		Container cp = j.getContentPane();
-		cp.add(jp,BorderLayout.CENTER);
-
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		j.setVisible(true);
-
-
 	}
 }
